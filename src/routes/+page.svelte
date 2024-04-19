f7eeb559aae93dde579ab44c6c7c49d6c5a0552e
Joshua861 <joshrl@proton.me>
2024-04-19 21:16:34 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<pre style="font-size: 24px;">
-  Usage: ph.joshrl.dev/[WIDTH]x[HEIGHT]
-  eg: ph.joshrl.dev/1920x1080
-</pre>
+<p>
+	Usage: ph.joshrl.dev/[WIDTH]x[HEIGHT]
+	<br /><br />
+	eg: ph.joshrl.dev/1920x1080
+</p>
+
+<style>
+	p {
+		font-size: 16px;
+		font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono',
+			'Courier New', monospace;
+	}
+</style>
